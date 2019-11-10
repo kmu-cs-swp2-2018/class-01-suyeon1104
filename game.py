28ece13730a7ef378d515d1bc18dbe9e1e17dcba
suyeon1104 <55120700+suyeon1104@users.noreply.github.com>
2019-11-10 17:16:15 +0900

    Update game.py
    주석 달고 출력 추가

--- game.py
+++ game.py
@@ -5,38 +5,48 @@ from word import Word
 
 def gameMain():
     word = Word('words.txt')
-    guess = Guess(word.randFromDB())
+    guess = Guess(word.randFromDB()) #랜덤 단어 선택
 
     finished = False
     hangman = Hangman()
-    maxTries = hangman.getLife()
+    maxTries = hangman.getLife() #목숨개수 초기화
 
     while guess.numTries < maxTries:
-
+        # numTries는 추측 실패 횟수
         display = hangman.get(maxTries - guess.numTries)
         print(display)
         guess.display()
 
         guessedChar = input('Select a letter: ')
+        # 입력받은 문자열이 1개의 문자가 아닐 때
         if len(guessedChar) != 1:
             print('One character at a time!')
             continue
+        # 입력받은 문자가 이미 이전에 입력받은 것일 떄
         if guessedChar in guess.guessedChars:
             print('You already guessed \"' + guessedChar + '\"')
             continue
-
+        #전체 문자 맞추면 while문 break 하고 Success 프린트
         finished = guess.guess(guessedChar)
         if finished == True:
             break
 
     if finished == True:
-        print('Success')
+        print()
+        print(guess.currentStatus)
+        print()
+        print('Success!')
+        print('You win by trying', guess.numTries, 'Times')
+    #허용된 횟수 내에 전체 문자 못 맞추면 hangman.text[0] 출력, 즉 목 매다는 그림 출력
     else:
         print(hangman.get(0))
         print('word [' + guess.secretWord + ']')
-        print('guess [' + guess.currentStatus + ']')
+        print('guess', end = '')
+        print(guess.currentStatus)
+        print()
         print('Fail')
-
+        print("The word was", guess.secretWord)
 
 if __name__ == '__main__':
     gameMain()
+
